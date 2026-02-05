c1b6df1c93d2f89b978ab13fa5c1eed7d6f9145a
vcm7000 <vcm7000@gmail.com>
2026-02-05 17:16:04 -0300

    reorganizando filtros por ordem alfabetica

--- src/data/categoriasMusculos.ts
+++ src/data/categoriasMusculos.ts
@@ -1,15 +1,12 @@
 export const categoriasMusculos = [{
-    categoria: 'Bíceps',
-    musculos: ['biceps_esq', 'biceps_dir', 'cabeça_curta_biceps_esq', 'cabeça_curta_biceps_dir']
-}, {
-    categoria: 'Tríceps',
-    musculos: ['cab_longa_triceps_esq', 'cab_lat_triceps_esq', 'cab_med_triceps_esq', 'cab_longa_triceps_dir', 'cab_lat_triceps_dir', 'cab_med_triceps_dir']
+    categoria: 'Abdômen',
+    musculos: ['abdomen', 'abdomen_inf', 'transverso_abd_esq', 'transverso_abd_dir']
 }, {
     categoria: 'Antebraço',
-    musculos: ['braquioradial_esq', 'braquioradial_dir'],
-    }, {
-    categoria: 'Peito',
-    musculos: ['peitoral_superior_esq', 'peitoral_superior_dir', 'peitoral_medial_esq', 'peitoral_medial_dir', 'peitoral_inferior_esq', 'peitoral_inferior_dir']
+    musculos: ['braquioradial_esq', 'braquioradial_dir']
+}, {
+    categoria: 'Bíceps',
+    musculos: ['biceps_esq', 'biceps_dir', 'cabeça_curta_biceps_esq', 'cabeça_curta_biceps_dir']
 }, {
     categoria: 'Costas',
     musculos: ['dorsal_esq', 'dorsal_dir', 'redondo_maior_esq', 'redondo_maior_dir', 'post_trapezio']
@@ -17,13 +14,15 @@ export const categoriasMusculos = [{
     categoria: 'Ombros',
     musculos: ['deltoide_anterior_esq', 'deltoide_anterior_dir', 'deltoide_post_esq', 'deltoide_post_dir', 'trapezio_esq', 'trapezio_dir']
 }, {
+    categoria: 'Panturrilha',
+    musculos: ['gastrocnemio_esq', 'gastrocnemio_dir', 'gastrocnemio_lat_esq', 'gastrocnemio_lat_dir', 'gastrocnemio_med_esq', 'gastrocnemio_med_dir', 'soleo_esq', 'soleo_dir']
+}, {
+    categoria: 'Peito',
+    musculos: ['peitoral_superior_esq', 'peitoral_superior_dir', 'peitoral_medial_esq', 'peitoral_medial_dir', 'peitoral_inferior_esq', 'peitoral_inferior_dir']
+}, {
     categoria: 'Pernas',
     musculos: ['vasto_lat_esq', 'vasto_lat_dir', 'vasto_medial_esq', 'vasto_medial_dir', 'reto_femoral_esq', 'reto_femoral_dir', 'biceps_femoral_esq', 'biceps_femoral_dir', 'semitendineo_esq', 'semitendineo_dir', 'gluteo']
 }, {
-    categoria: 'Abdômen',
-    musculos: ['abdomen', 'abdomen_inf', 'transverso_abd_esq', 'transverso_abd_dir']
-}, {
-    categoria: 'Panturrilha',
-    musculos: ['gastrocnemio_esq', 'gastrocnemio_dir', 'gastrocnemio_lat_esq', 'gastrocnemio_lat_dir', 'gastrocnemio_med_esq', 'gastrocnemio_med_dir', 'soleo_esq', 'soleo_dir']
-
+    categoria: 'Tríceps',
+    musculos: ['cab_longa_triceps_esq', 'cab_lat_triceps_esq', 'cab_med_triceps_esq', 'cab_longa_triceps_dir', 'cab_lat_triceps_dir', 'cab_med_triceps_dir']
 }];
